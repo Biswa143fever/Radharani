48c4d3b478a196773aa3ecc2b8100886d0b8e454
biswaranjan.sahu <biswaranjan.sahu@SVINDK00008.SPI.COM>
2018-10-08 17:00:49 +0530

    Commiting File level changes

--- Radharani/src/main/java/Mira.java
+++ Radharani/src/main/java/Mira.java
@@ -4,6 +4,7 @@ public class Mira {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		System.out.println("SOFTVISION A CTS Company Wel comes  BTAT Statement");
 
 	}
 
